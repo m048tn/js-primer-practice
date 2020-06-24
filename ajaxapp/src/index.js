10827be7fa0cb3f3f2dc3bf84f9c36895557d8d2
eco <46394706+TanakaMoeko@users.noreply.github.com>
2020-06-24 22:36:57 +0900

    user情報を画面に表示する

--- ajaxapp/src/index.js
+++ ajaxapp/src/index.js
@@ -8,6 +8,19 @@ function fetchUserInfo() {
         console.error("エラーレスポンス", response);
       } else {
         return response.json().then((userInfo) => {
+          const view = escapeHTML`
+          <h4>${userInfo.avater_url} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+            <dt>location</dt>
+            <dd>${userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+
+          const result = document.getElementById("result");
+          result.innerHTML = view;
           // パースされたオブジェクトが渡される
           console.log(userInfo);
         });
@@ -16,4 +29,30 @@ function fetchUserInfo() {
     .catch((error) => {
       console.error(error);
     });
+}
+
+/**
+ * テンプレートリテラルにタグ付けするための関数
+ *
+ * @param {string} strings 文字列リテラルの配列
+ * @param  {...any} values 埋め込まれる値の配列
+ */
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
