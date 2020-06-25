00757deadf517a36fae84d8ddf95e934492e00e0
eco <46394706+TanakaMoeko@users.noreply.github.com>
2020-06-25 23:03:59 +0900

    refactor nodecli app

--- nodecli/src/main.js
+++ nodecli/src/main.js
@@ -1,8 +1,16 @@
-const myModule = require("./my-module.js");
 const commander = require("commander");
+const fs = require("fs");
 
 commander.parse(process.argv);
 const filePath = commander.args[0];
-// console.log(process.argv);
-console.log(myModule.foo);
 console.log(filePath);
+
+fs.readFile(filePath, { encoding: "utf-8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  } else {
+    console.log(file);
+  }
+});
