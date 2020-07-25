4b20aa7f39a03f313009511bc3623064376ebefa
m048tn <m048smsnm.tn@gmail.com>
2020-07-25 12:44:54 +0900

    fix

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,4 +5,13 @@ export class App {
     console.log("App initialized");
     console.log(this);
   }
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    formElement.addEventListener("submit", (event) => {
+      // submitイベントの本来の動作を止める
+      event.preventDefault();
+      console.log(`入力値の値：${inputElement.value}`);
+    });
+  }
 }
